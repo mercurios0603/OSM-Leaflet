2f642718276f533016695b99a1e42c387bdc0fea
mercurios0603 <mercurios0603@gmail.com>
2023-12-17 20:35:16 +0900

    osm01

---
+++ src/main/java/com/example/LeafletMap/LeafletMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeafletMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafletMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafletMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeafletMap/LeafletMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeafletMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafletMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
